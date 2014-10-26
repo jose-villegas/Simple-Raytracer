d5d40883c091705e252b4fa9f45daea8a4ea3140
jose-villegas <polonoise@gmail.com>
2014-10-26 18:10:14 -0430

    Serializing data from input

--- FGC_T4/Main.cpp
+++ FGC_T4/Main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include "SceneParser.h"
+#include "Scene.h"
 
 int main(int argc, char *argv[])
 {
     Scene::Parser parser(std::ifstream("test.yml"));
+    Scene::SceneManager sceneElements = Scene::Serializer::serializeParsedScene(parser);
+    // Wait for User input to close
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); std::cin.get();
     return 0;
 }
